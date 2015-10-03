0a1864ce8793765f9820901d85b23779b3a3c5b9
Lem-on <petr.smoldas@centrum.cz>
2015-10-03 21:19:05 +0200

    Update autodub.js

--- autodub.js
+++ autodub.js
@@ -1,11 +1,6 @@
-icon-arrow-up
-var yo = "AutoDub - Načten!";
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+API.on(API.ADVANCE, aw); /* autodub */
+
+/* autodub */
+function aw(){ 
+	$("#dubup voted").click(); 
 }
-var num = getRandomArbitrary(3000,6000);
-API.sendChat(yo);
-$("#icon-arrow-up").click();
-API.on(API.ADVANCE, autowoot);
-setInterval(function() { API.sendChat(yo); }, 1800000);
-function autowoot(){ setTimeout(function() {$("#icon-arrow-up").click();}, num);  }
